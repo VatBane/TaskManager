71fc6282ef30b6127dd54a7f1f27c33ee9fe8337
VatBane <70464844+VatBane@users.noreply.github.com>
2022-07-06 12:45:26 +0300

    tasks router

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
 
-app.use(express.static('./public'))
+// middleware
+app.use(express.static("./public"));
 
 // routes
 app.get("/", (req, res) => {
   res.status(200);
 });
 
-app.all('*', (req, res)=>{
-  res.status(404).send('Not Found');
-})
+app.use("/api/v1/tasks", tasks);
+
+app.all("*", (req, res) => {
+  res.status(404).send("Not Found");
+});
 
 const port = 8080;
-app.listen(port, console.log('Server successfully started...'))
+app.listen(port, console.log("Server successfully started..."));
