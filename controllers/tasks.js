eeb180daeab40e8ec96ad75f82d8d95cc1f06bb1
VatBane <70464844+VatBane@users.noreply.github.com>
2022-07-06 12:50:08 +0300

    tasks controller no db setup

---
+++ controllers/tasks.js
@@ -0,0 +1,22 @@
+const getAllTasks = (req, res) => {
+  res.status(200).send('all items')
+}
+
+const createTask = (req, res) => {
+
+}
+
+const updateTask = (req, res) => {
+
+}
+
+const deleteTask = (req, res) => {
+
+}
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  updateTask,
+  deleteTask
+}
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getAllTasks,
+  createTask,
+  updateTask,
+  deleteTask
+} = require('../controllers/tasks')
 
-router.route('/').get((req, res)=>{
-  res.send('all items')
-})
+router.route('/').get(getAllTasks)
 
 module.exports = router;
