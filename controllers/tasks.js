99eea5614acf905705379c615d4b708d6bda2e8a
VatBane <70464844+VatBane@users.noreply.github.com>
2022-07-06 12:55:49 +0300

    tasks controller no db all functions

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,20 +2,25 @@ const getAllTasks = (req, res) => {
   res.status(200).send('all items')
 }
 
-const createTask = (req, res) => {
+const getTask = (req, res) => {
+  res.status(200).send('single item')
+}
 
+const createTask = (req, res) => {
+  res.status(200).send('task created')
 }
 
 const updateTask = (req, res) => {
-
+  res.status(200).send('task updated')
 }
 
 const deleteTask = (req, res) => {
-
+  res.status(200).send('task deleted')
 }
 
 module.exports = {
   getAllTasks,
+  getTask,
   createTask,
   updateTask,
   deleteTask
