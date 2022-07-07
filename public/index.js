272d03df838811e9f05daac5d750516885485a37
VatBane <70464844+VatBane@users.noreply.github.com>
2022-07-07 17:14:09 +0300

    prev fix

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById('submit-button');
 
 btn.addEventListener('click', () => {
-  task = document.getElementById('task-field').value;
+  const task = document.getElementById('task-field').value;
 })
