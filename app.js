f8ba1729a74fe85be9871d6d9438e01f31fda02a
VatBane <70464844+VatBane@users.noreply.github.com>
2022-07-07 17:13:51 +0300

    basic front end

--- app.js
+++ app.js
@@ -9,14 +9,10 @@ app.use(express.static("./public"));
 app.use(express.json())
 
 // routes
-app.get("/", (req, res) => {
-  res.status(200);
-});
-
 app.use("/api/v1/tasks", tasks);
 
 app.all("*", (req, res) => {
-  res.status(404).send("Not Found");
+   res.status(404).send("Not Found");
 });
 
 const port = 8080;
---
+++ public/index.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('submit-button');
+
+btn.addEventListener('click', () => {
+  task = document.getElementById('task-field').value;
+})
